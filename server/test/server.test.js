774331eb7893624e41ae4d68260a069bb5d57af8
tumtumgigi <tosapol.engr@gmail.com>
2018-12-30 21:52:00 +0700

    Add tests for GET /todos

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -5,12 +5,22 @@ const request = require('supertest');
 const {app} = require('./../server'); // ../ go back one level to server
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+    text: 'First test todo'
+}, {
+    text: 'Second test todo'
+}];
+
+
+
 // Make sure the database is empty
 beforeEach((done) => {
-    Todo.remove({}).then(() => done()); // Expression Syntax, clear all the db before request
+    Todo.deleteMany({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done()); // Expression Syntax, clear all the db before request
 });
 
-// Group the routes
+// Group the POST routes
 describe('POST /todos', () => { // Error Function
     it('should create a new todo', (done) => { // Callback Function because it'll be asynchronous test
     // Create the testing text    
@@ -31,7 +41,7 @@ describe('POST /todos', () => { // Error Function
                 }
 
                 // Fetch all todos in collection
-                Todo.find().then((todos) => { // Database is correct
+                Todo.find({text}).then((todos) => { // Database is correct
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -52,9 +62,22 @@ describe('POST /todos', () => { // Error Function
                 }
 
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0); // Expect writen data to be empty
+                    expect(todos.length).toBe(2); // Expect writen data to be empty
                     done();
                 }).catch((e) => done(e));
             });
     });
 });
+
+// Group the GET routes
+describe('GET /todos', () => {
+    it('should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);
+    })
+})
