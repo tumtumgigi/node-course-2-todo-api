852045ee44f91fe9753c6ea0e7c2a82d0a16e4a8
tumtumgigi <tosapol.engr@gmail.com>
2019-01-05 16:59:31 +0700

    Add POST /users route and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,62 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-var User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
         trim: true,
-        minlength: 1
-    }
+        minlength: 1,
+        unique: true,
+        validate: {
+            // Custom function form
+            // validator: (value) => {
+            //     return validator.isEmail(value);
+            // },
+            validator: validator.isEmail, // Use this line instead of above
+            message: '{VALUE} is not a valid email'
+        }
+    },
+    password: {
+        type: String,
+        require: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
+
+// What exactly is send back when mongoose convert to JSON value
+UserSchema.methods.toJSON = function () {
+    var user = this;
+    var userObject = user.toObject();
+
+    return _.pick(userObject, ['_id', 'email']);
+};
+
+// Create a methods for UserSchema Object
+UserSchema.methods.generateAuthToken = function () {
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+    user.tokens = user.tokens.concat([{access, token}]);
+
+    return user.save().then(() => {
+        return token;
+    });
+};
+
+var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
